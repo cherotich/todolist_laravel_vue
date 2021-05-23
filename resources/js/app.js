203486adfd5586a5cc3b789f8f6126b869779348
cherotich <carolcaro60@gmail.com>
2021-05-23 21:28:25 +0300

    Added and rendered UI components

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,7 +5,15 @@
  */
 
 require('./bootstrap');
+import Vue from 'vue';
+import App from './vue/app'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPlusSquare,faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
+library.add(faPlusSquare,faTrash)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 window.Vue = require('vue');
 
 /**
@@ -29,4 +37,8 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    components : {
+        App
+    }
 });
+ 
